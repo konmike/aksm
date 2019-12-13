f1fdf287e7de6a3c5f6303334141cd3953ea9529
konmike <konecmi4@fit.cvut.cz>
2019-12-13 22:04:12 +0100

    Doplneno logo, menu, vypis konkretniho prispevku

--- index.php
+++ index.php
@@ -2,7 +2,25 @@
 
 <main>
 
-<h1>AKSM</h1>
+    <header class="header header--search-and-nav">
+        <a href="./"><img src="wp-content/uploads/2019/12/logo2.png" alt="AKSM" width="50px"></a>
+
+		<?php wp_nav_menu(  array( 'theme_location' => 'primary-menu',
+		                           'container' => 'nav',
+		                           'container_class' => 'nav nav--block nav--primary menu-fixed',
+		                           'container_id' => 'menu'));  ?>
+
+    </header>
+
+    <?php
+        $introducePost = get_post(30);
+        $title = $introducePost->post_title;
+        $content = $introducePost->post_content;
+    ?>
+    <div class="article">
+        <h1><?=$title?></h1>
+        <p><?=$content?></p>
+    </div>
 
 </main>
 
