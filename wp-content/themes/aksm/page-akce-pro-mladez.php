ffb3215308e36f170f8fcf890ade52ac1416f144
konmike <konecmi4@fit.cvut.cz>
2021-03-31 07:21:49 +0200

    Upload posts from specific cat

--- wp-content/themes/aksm/page-akce-pro-mladez.php
+++ wp-content/themes/aksm/page-akce-pro-mladez.php
@@ -5,14 +5,28 @@
         <h1>Akce pro mládež</h1>
 
         <section class="section section--projects">
-            <?php echo do_shortcode('[PGP id=75]') ?>
-            <?php echo do_shortcode('[PGP id=59]') ?>
+            <?php // echo do_shortcode('[PGP id=75]') ?>
+            <?php // echo do_shortcode('[PGP id=59]') ?>
 
-	        <?php if (have_posts()) : ?>
-		        <?php while (have_posts()) : the_post(); ?>
-                        <?php the_content(); ?>
-		        <?php endwhile; ?>
-	        <?php endif; ?>
+            <?php
+            $the_query = new WP_Query( array( 'cat' => 5 ) );
+ 
+            // The Loop
+            if ( $the_query->have_posts() ) {
+                echo '<ul>';
+                while ( $the_query->have_posts() ) {
+                    $the_query->the_post();
+                    echo '<li>' . get_the_title() . '</li>';
+                    echo '<li>' . the_post_thumbnail() . '</li>';
+                }
+                echo '</ul>';
+            } else {
+                // no posts found
+            }
+            /* Restore original Post Data */
+            wp_reset_postdata();
+            ?>
+	         
 
         </section>
 
