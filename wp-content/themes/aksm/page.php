0b121e609933bde9972292cfa4e7bf4f68d9b77a
Michal Konecny <michalkonecny10@gmail.com>
2024-04-08 08:23:53 +0200

    Update page.php and _base.scss files

--- wp-content/themes/aksm/page.php
+++ wp-content/themes/aksm/page.php
@@ -1,13 +1,17 @@
 <?php get_header(); ?>
-	<div id="content" class="page page--general">
-		<?php if (have_posts()) :  while (have_posts()) : the_post(); ?>
+<div id="content" class="page page--general">
+	<?php if (have_posts()):
+		while (have_posts()):
+			the_post(); ?>
 			<section class="section section--general">
-				<h1><?php the_title(); ?></h1>
+				<h1 class="title">
+					<?php the_title(); ?>
+				</h1>
 				<?php the_content(); ?>
 			</section>
-		<?php endwhile; 
-		endif;
-		?>
-		
-	</div>
+		<?php endwhile;
+	endif;
+	?>
+
+</div>
 <?php get_footer(); ?>
